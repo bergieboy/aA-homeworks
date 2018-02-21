b309e03bae30c1ec2fe43cf9bc291d92d09d10ad
Todd <toddleebergman@gmail.com>
2018-02-20 21:06:49 -0800

    Final commit for w1d3 homework

--- W2D3/dessert/spec/dessert_spec.rb
+++ W2D3/dessert/spec/dessert_spec.rb
@@ -7,36 +7,68 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 
 describe Dessert do
   let(:chef) { double("chef") }
+  let(:cookie) { Dessert.new("Cookie", 12, chef) }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(cookie.type).to eq("Cookie")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(cookie.quantity).to eq(12)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(cookie.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("Cookie", "12", "chef") }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      cookie.add_ingredient("Chocolate Chips")
+      expect(cookie.ingredients).to include("Chocolate Chips")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = ["flour", "eggs", "chocolate chips", "sugar"]
+
+      ingredients.each { |part| cookie.add_ingredient(part) }
+
+      expect(cookie.ingredients).to eq(ingredients)
+      cookie.mix!
+      expect(cookie.ingredients).not_to eq(ingredients)
+      expect(cookie.ingredients.sort).to eq(ingredients.sort)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      cookie.eat(5)
+      expect(cookie.quantity).to eq(7)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { cookie.eat(120) }.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Todd")
+      expect(cookie.serve).to eq("Chef Todd has made 12 Cookies!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(cookie)
+      cookie.make_more
+    end
   end
 end
