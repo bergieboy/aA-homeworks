66809a2466c29f9de4ba19dfd3d1cdb6908be7ef
Todd <toddleebergman@gmail.com>
2018-02-26 22:23:00 -0800

    Updated plays and playwrights classes

--- W3D2/plays_playwrites/plays.rb
+++ W3D2/plays_playwrites/plays.rb
@@ -19,6 +19,33 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+    SELECT
+      *
+    FROM
+      plays
+    WHERE
+      title = ?
+    SQL
+    return nil unless play.length > 0
+    Play.new(play.first)
+  end
+
+  def self.find_by_playwright(name)
+    playwright = Playwright.find_by_name(name)
+    plays = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
+    SELECT
+      *
+    FROM
+      plays
+    WHERE
+      playwright_id = ?
+    SQL
+
+    plays.map { |play| Play.new(play)}
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -48,11 +75,59 @@ class Play
         id = ?
     SQL
   end
+end
 
-  def find_by_title(title)
+class Playwright
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
   end
 
-  def find_by_playwright(name)
+  def self.find_by_name(name)
+    playwrights = PlayDBConnection.instance.execute(<<-SQL, name)
+    SELECT
+      *
+    FROM
+      plays
+    WHERE
+      name = ?
+    SQL
+    return nil unless playwrights.length > 0
+
+    Playwright.new(playwrights.first)
   end
 
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    
+  end
 end
