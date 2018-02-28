07e9c00c621926907f7b48b188e0c37835547a13
Todd <toddleebergman@gmail.com>
2018-02-27 22:52:19 -0800

    Final homwork commits

--- W3D3/aA_hw_project/app/models/house.rb
+++ W3D3/aA_hw_project/app/models/house.rb
@@ -1,3 +1,9 @@
-class House
+class House < ApplicationRecord
+  validates :address, presence: true
+
+  has_many :residence,
+    primary_key: :id,
+    foreign_key: :house_id,
+    class_name: :Person
 
 end
