a629791aab93fad8752b665af0d00a4dd40eaade
Todd <toddleebergman@gmail.com>
2018-03-26 21:51:17 -0700

    Final commit for W7D2

--- W7D2/todo_middleware/frontend/store/store.js
+++ W7D2/todo_middleware/frontend/store/store.js
@@ -2,11 +2,28 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(addLoggingToDispatch, sillyMiddleware)
+  );
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
 }
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+// Bonus Phase: Add an extra middleware!
+const sillyMiddleware = store => next => action => {
+  console.log("Silly");
+  next(action);
+};
 
 export default configureStore;
